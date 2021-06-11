d6836eddea8e8842be396971a01be9c6aeab0e46
dojo2021-r-nakaoka <nakaoka@seplus2016.onmicrosoft.com>
2021-06-11 17:20:02 +0900

    中岡_修正中_2021/06/11

--- D4_situmonn/src/dao/QuestionsDao.java
+++ D4_situmonn/src/dao/QuestionsDao.java
@@ -6,17 +6,20 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
-import java.util.Date;
+//import java.util.Date;
+import java.sql.Date;
 
 import model.Question;
 
 public class QuestionsDao {
 
+	//質問一覧検索時(解決済み)
 	//引数paramで検索項目を指定し、検索結果のリストを返す
-	public List<Question> select(Question param) {
+	public List<Question> selectQListS(Question param) {
 		Connection conn = null;
-		List<Question> cardList = new ArrayList<Question>();
+		List<Question> QList = new ArrayList<Question>();
 
 		try {
 			//JDBCドライバ読み込み
@@ -26,32 +29,56 @@ public class QuestionsDao {
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/D-4/QAsystem", "sa", "sa");
 
 			//SQL文を準備
-			String sql = "select Q_ID, Q_TITLE, Q_CONTENTS, Q_TAG01, Q_TAG02, Q_TAG03, Q_TAG04, Q_TAG05, USER_ID, Q_FILE, Q_DATE, DONE_TAG, COUNTER "
+			String sql = "select D_QUESTIONS.Q_ID, D_QUESTIONS.Q_TITLE, D_QUESTIONS.Q_CONTENTS, "
+					+ "D_QUESTIONS.Q_TAG01, D_QUESTIONS.Q_TAG02, D_QUESTIONS.Q_TAG03, D_QUESTIONS.Q_TAG04, D_QUESTIONS.Q_TAG05, "
+					+ "D_QUESTIONS.USER_ID, D_QUESTIONS.Q_FILE, D_QUESTIONS.Q_DATE, D_QUESTIONS.DONE_TAG, D_QUESTIONS.COUNTER, M_USERS.USER_NAME "
 					+ "from D_QUESTIONS "
-					+ "where Q_ID = ? and USER_ID = ? "
+					+ "inner join M_USERS "
+					+ "on M_USERS.USER_ID = D_QUESTIONS.USER_ID "
+					+ "where D_QUESTIONS.Q_TAG01"
 					+ "order by Q_DATE desc";
+		}
+	}
+
+	//質問一覧検索時(未解決)
+	//引数paramで検索項目を指定し、検索結果のリストを返す
+	public List<Question> selectQListU(Question param) {
+
+	}
+
+
+	//検索一覧から質問詳細移動時 & マイページから質問詳細移動時用
+	//引数paramで検索項目を指定し、検索結果のリストを返す
+	public List<Question> selectQDetail(String q_id) {
+		Connection conn = null;
+		List<Question> QEdit = new ArrayList<Question>();
+
+		try {
+			//JDBCドライバ読み込み
+			Class.forName("org.h2.Driver");
+
+			//データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/D-4/QAsystem", "sa", "sa");
+
+			//SQL文を準備
+			String sql = "select D_QUESTIONS.Q_ID, D_QUESTIONS.Q_TITLE, D_QUESTIONS.Q_CONTENTS, "
+					+ "D_QUESTIONS.Q_TAG01, D_QUESTIONS.Q_TAG02, D_QUESTIONS.Q_TAG03, D_QUESTIONS.Q_TAG04, D_QUESTIONS.Q_TAG05, "
+					+ "D_QUESTIONS.USER_ID, D_QUESTIONS.Q_FILE, D_QUESTIONS.Q_DATE, D_QUESTIONS.DONE_TAG, M_USERS.USER_NAME "
+					+ "from D_QUESTIONS "
+					+ "inner join M_USERS "
+					+ "on M_USERS.USER_ID = D_QUESTIONS.USER_ID "
+					+ "where Q_ID = ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			//SQL文を完成させる
-			if (param.getQ_id() != 0) {
-				pStmt.setInt(1, param.getQ_id());
-			}
-			else {
-				pStmt.setInt(1, 0);
-			}
-			if (param.getUser_id() != null && !param.getUser_id().equals("")) {
-				pStmt.setString(2, "%" + param.getUser_id() + "%");
-			}
-			else {
-				pStmt.setString(2, "%");
-			}
+			pStmt.setString(1, q_id);
 
 			//SQL文を実行し、結果表を取得する
 			ResultSet rs = pStmt.executeQuery();
 
 			//結果表をコレクションにコピーする
 			while (rs.next()) {
-				Question card = new Question(
+				Question QEditList = new Question(
 						rs.getInt("Q_ID"),
 						rs.getString("Q_TITLE"),
 						rs.getString("Q_CONTENTS"),
@@ -64,18 +91,19 @@ public class QuestionsDao {
 						rs.getString("Q_FILE"),
 						rs.getDate("Q_DATE"),
 						rs.getInt("DONE_TAG"),
-						rs.getInt("COUNTER")
+						rs.getInt("COUNTER"),
+						rs.getString("USER_NAME")
 						);
-				cardList.add(card);
+				QEdit.add(QEditList);
 			}
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
-			cardList = null;
+			QEdit = null;
 		}
 		catch (ClassNotFoundException e) {
 			e.printStackTrace();
-			cardList = null;
+			QEdit = null;
 		}
 		finally {
 			//データベースを切断
@@ -85,21 +113,23 @@ public class QuestionsDao {
 				}
 				catch (SQLException e) {
 					e.printStackTrace();
-					cardList = null;
+					QEdit = null;
 				}
 			}
 		}
 
 		//結果を返す
-		return cardList;
+		return QEdit;
 	}
 
-	//引数cardで指定されたレコードを登録し、成功したらtrueを返す
-	public boolean insert(Question card) {
+	//質問投稿用
+	//引数paramで指定されたレコードを登録し、成功したらtrueを返す
+	public boolean insertQRecord(Question param) {
 		Connection conn = null;
-		boolean result = false;
+		boolean insertResult = false;
 
 		try {
+
 			//JDBCドライバ読み込み
 			Class.forName("org.h2.Driver");
 
@@ -132,79 +162,117 @@ public class QuestionsDao {
 
 			//INSERT文を準備
 			String sql = "insert into D_QUESTION ( Q_ID, Q_TITLE, Q_CONTENTS, Q_TAG01, Q_TAG02, Q_TAG03, Q_TAG04, Q_TAG05, USER_ID, Q_FILE, Q_DATE, DONE_TAG, COUNTER ) "
-					+ "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+					+ "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURDATE(), 0, 0)";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			//SQL文を完成させる
 			pStmt.setInt(1,  maxID);
-			if (card.getQ_title() != null && !card.getQ_title().equals("")) {
-				pStmt.setString(2, card.getQ_title());
-			}
-			else {
-				pStmt.setString(2, null);
-			}
-			if (card.getQ_contents() != null && !card.getQ_contents().equals("")) {
-				pStmt.setString(3, card.getQ_contents());
-			}
-			else {
-				pStmt.setString(3, null);
-			}
-			if (card.getQ_tag01() != null && !card.getQ_tag01().equals("")) {
-				pStmt.setString(4, card.getQ_tag01());
+			pStmt.setString(2, param.getQ_title());
+			pStmt.setString(3, param.getQ_contents());
+			if (param.getQ_tag01() != null && !param.getQ_tag01().equals("")) {
+				pStmt.setString(4, param.getQ_tag01());
 			}
 			else {
 				pStmt.setString(4, null);
 			}
-			if (card.getQ_tag02() != null && !card.getQ_tag02().equals("")) {
-				pStmt.setString(5, card.getQ_tag02());
+			if (param.getQ_tag02() != null && !param.getQ_tag02().equals("")) {
+				pStmt.setString(5, param.getQ_tag02());
 			}
 			else {
 				pStmt.setString(5, null);
 			}
-			if (card.getQ_tag03() != null && !card.getQ_tag03().equals("")) {
-				pStmt.setString(6, card.getQ_tag03());
+			if (param.getQ_tag03() != null && !param.getQ_tag03().equals("")) {
+				pStmt.setString(6, param.getQ_tag03());
 			}
 			else {
 				pStmt.setString(6, null);
 			}
-			if (card.getQ_tag04() != null && !card.getQ_tag04().equals("")) {
-				pStmt.setString(7, card.getQ_tag04());
+			if (param.getQ_tag04() != null && !param.getQ_tag04().equals("")) {
+				pStmt.setString(7, param.getQ_tag04());
 			}
 			else {
 				pStmt.setString(7, null);
 			}
-			if (card.getQ_tag05() != null && !card.getQ_tag05().equals("")) {
-				pStmt.setString(8, card.getQ_tag05());
+			if (param.getQ_tag05() != null && !param.getQ_tag05().equals("")) {
+				pStmt.setString(8, param.getQ_tag05());
 			}
 			else {
 				pStmt.setString(8, null);
 			}
-			if (card.getUser_id() != null && !card.getUser_id().equals("")) {
-				pStmt.setString(9, card.getUser_id());
-			}
-			else {
-				pStmt.setString(9, null);
-			}
-			if (card.getQ_file() != null && !card.getQ_file().equals("")) {
-				pStmt.setString(10, card.getQ_file());
+			pStmt.setString(9, param.getUser_id());
+			if (param.getQ_file() != null && !param.getQ_file().equals("")) {
+				pStmt.setString(10, param.getQ_file());
 			}
 			else {
 				pStmt.setString(10, null);
 			}
-			//2021/06/11 ここから
-			if (card.getQ_date() != null && !card.getQ_date().equals("")) {
-				pStmt.setDate(11, card.getQ_date());
-			}
-			else {
-				pStmt.setString(11, null);
-			}
-			if (card.getQ_title() != null && !card.getQ_title().equals("")) {
-				pStmt.setString(2, card.getQ_title());
-			}
-			else {
-				pStmt.setString(2, null);
-			}
 
+			//SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				insertResult = true;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			//データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		//結果を返す
+		return insertResult;
+	}
+
+	//質問詳細移動時のカウント回数+1
+	//引数q_idで指定したレコードのカウンターの回数を最大値+1に更新し、成功したらtrueを返す
+	public boolean updateCountup(int q_id) {
+
+		Connection conn = null;
+		boolean updateResult = false;
+
+		try {
+
+			//JDBCドライバ読み込み
+			Class.forName("org.h2.Driver");
+
+			//データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/D-4/QAsystem", "sa", "sa");
+
+			//2021/06/14　ここからスタート
+			//SQL文の準備
+			String sql = "select COUNTER from D_QUESTION where = ?";
+
+			//SQL文を完成させる
+			pStmt.setInt(1, q_id);
+
+			//SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			//UPDATE文を準備
+			String sql2 = "update D_QUESTIONS "
+					+ "set COUNTER = ? "
+					+ "where Q_ID = ?";
+			PreparedStatement pStmt2 = conn.prepareStatement(sql2);
+
+			//SQL文を完成させる
+			pStmt.setInt(1,  maxID);
+			pStmt.setInt(1,  maxID);
+
+			//SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				updateResult = true;
+			}
 		}
 	}
 }
