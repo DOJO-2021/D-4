2b55c0be639d86eca3d13423a5beacaf86719e2d
dojo2021-miyota <miyota@seplus2016.onmicrosoft.com>
2021-06-14 11:00:14 +0900

    a

--- D4_situmonn/src/Servlet/AnswersDetailServlet.java
+++ D4_situmonn/src/Servlet/AnswersDetailServlet.java
@@ -1,6 +1,9 @@
 package Servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +12,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import dao.BcDAO;
+import model.Bc;
+import model.Question;
 
 /**
  * Servlet implementation class AnswersDetailServlet
@@ -45,4 +52,19 @@ public class AnswersDetailServlet extends HttpServlet {
 			response.sendRedirect("/D4_situmonn/LoginServlet");
 			return;
 		}
+
+
+		//検索処理を行う
+		QuestionsDao qDao = new qDAO();
+		List<Question> QEdit = qDao.selectQDetail(q_id);
+
+		//リクエストスコープに格納する
+		request.setAttribute("listOfAll", listOfAll);
+
+
+		//list.jspにフォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/list.jsp");
+		dispatcher.forward(request, response);
+	}
+}
 }
