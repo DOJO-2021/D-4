a8944c002c26c31534f284035701ce42d14a6989
dojo2021-y-fujiwara <fujiwara@seplus.onmicrosoft.com>
2021-06-15 14:32:40 +0900

    Mypage仮完了

--- D4_situmonn/src/Servlet/MypageServlet.java
+++ D4_situmonn/src/Servlet/MypageServlet.java
@@ -40,11 +40,11 @@ public class MypageServlet extends HttpServlet {
 		// 検索結果をリクエストスコープに格納する
 		request.setAttribute("cardList", cardList);
 
-	    //リスエスとスコープに保存された検索結果のリストを取得
+	    //リスエスとスコープに保存された検索結果のリストを取得【保留：user_idは二回使えない→変えてもよいのか？2021/06/15】
 		QuestionsDao QDao = new QuestionsDao();
-		String user_id = (String) session.getAttribute("id");
-		System.out.println("ユーザID：" + user_id);
-		List<Question> QList = QDao.selectMyQList(user_id);
+		String id = (String) session.getAttribute("id");
+		System.out.println("ユーザID：" + id);
+		List<Question> QList = QDao.selectMyQList(id);
 
 		// 検索結果をリクエストスコープに格納する
 	 	request.setAttribute("QList", QList);
